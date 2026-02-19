7ea9606b40ef9b62d1dab07216023165194a9a9e
wiciu <wtezycki1@gmail.com>
2026-02-19 18:19:45 +0100

    chore(build): initialize Spring Boot project with hexagonal package structure for Offer module

---
+++ src/main/java/pl/hyrion/hyrionbackend/HyrionBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.hyrion.hyrionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyrionBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyrionBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/hyrion/hyrionbackend/HyrionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.hyrion.hyrionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyrionBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
