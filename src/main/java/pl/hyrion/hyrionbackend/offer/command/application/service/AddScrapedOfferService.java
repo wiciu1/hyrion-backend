19f5235c0001738387ac9e76861562bc29d6a39d
wiciu <wtezycki1@gmail.com>
2026-02-20 19:49:41 +0100

    in and out ports for service adding scraped offer to repository

---
+++ src/main/java/pl/hyrion/hyrionbackend/offer/command/application/service/AddScrapedOfferService.java
@@ -0,0 +1,55 @@
+package pl.hyrion.hyrionbackend.offer.command.application.service;
+
+import lombok.RequiredArgsConstructor;
+import pl.hyrion.hyrionbackend.offer.command.application.port.in.AddScrapedOfferUseCase;
+import pl.hyrion.hyrionbackend.offer.command.application.port.out.JobOfferRepositoryPort;
+import pl.hyrion.hyrionbackend.offer.command.domain.JobOffer;
+import pl.hyrion.hyrionbackend.offer.command.domain.Salary;
+import pl.hyrion.hyrionbackend.offer.command.domain.enums.ExperienceLevel;
+import pl.hyrion.hyrionbackend.offer.command.domain.enums.SourcePlatform;
+
+@RequiredArgsConstructor
+public class AddScrapedOfferService implements AddScrapedOfferUseCase {
+
+    private final JobOfferRepositoryPort jobOfferRepositoryPort;
+
+    @Override
+    public void addOffer(AddOfferCommand command) {
+        // Translating raw data to Value Objects and Enums
+        Salary salary = createSalary(command);
+        ExperienceLevel experienceLevel = ExperienceLevel.valueOf(command.experienceLevel().toUpperCase());
+        SourcePlatform sourcePlatform = SourcePlatform.valueOf(command.sourcePlatform().toUpperCase());
+
+        // Create aggregate:
+        JobOffer jobOffer = JobOffer.builder()
+                .title(command.title())
+                .url(command.url())
+                .location(command.location())
+                .salary(salary)
+                .skills(command.skills())
+                .isRemote(command.isRemote())
+                .createdAt(command.createdAt())
+                .validTo(command.createdAt())
+                .originalId(command.originalId())
+                .experienceLevel(experienceLevel)
+                .sourcePlatform(sourcePlatform)
+                .build();
+
+        jobOfferRepositoryPort.save(jobOffer);
+    }
+
+    private Salary createSalary(AddOfferCommand command) {
+        if (command.minSalary() != null && command.minSalary().equals(command.maxSalary())) {
+            return Salary.exact(
+                    command.minSalary(),
+                    command.currency(),
+                    command.paymentSchedule());
+        }
+        return Salary.range(
+                command.minSalary(),
+                command.maxSalary(),
+                command.currency(),
+                command.paymentSchedule()
+        );
+    }
+}
