1fdda7d5dec0e366ae6767ab249d74dc88bc386c
wiciu <wtezycki1@gmail.com>
2026-02-21 09:10:31 +0100

    Command side adapter to save offers; h2 db configuration

---
+++ src/main/java/pl/hyrion/hyrionbackend/offer/command/adapter/out/persistence/JobOfferEntity.java
@@ -0,0 +1,57 @@
+package pl.hyrion.hyrionbackend.offer.command.adapter.out.persistence;
+
+// Database entity for Job Offer
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "job_offers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JobOfferEntity {
+
+    @Id
+    private UUID id;
+
+    private String title;
+    private String url;
+    private String location;
+
+    // Unpacking salary
+    private BigDecimal minSalary;
+    private BigDecimal maxSalary;
+    private String currency;
+    private String paymentSchedule;
+
+    // Relationship 1:N for list of skills
+    // job_offer_skills
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(
+            name = "job_offer_skills",
+            joinColumns = @JoinColumn(name = "job_offer_id"))
+    @Column(name = "skill")
+    private Set<String> skills;
+
+    private boolean isRemote;
+    private LocalDateTime createdAt;
+    private LocalDateTime validTo;
+    private String originalId;
+
+    @Column(name = "experience_level")
+    private String experienceLevel;
+
+    @Column(name = "source_platform")
+    private String sourcePlatform;
+
+    @Column(name = "status")
+    private String status;
+}
